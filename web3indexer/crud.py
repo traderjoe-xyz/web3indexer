e6226c5f68613200d6260eea0d911f68e70135f2
jummy123 <jummy123@users.noreply.github.com>
2022-03-08 20:57:03 +0000

    Avoid unbounded array in document

--- web3indexer/crud.py
+++ web3indexer/crud.py
@@ -6,13 +6,11 @@ log = structlog.get_logger()
 
 def insert_if_not_exists(db, address, abi):
     contract = db.nft_contracts.find_one({'address': address})
-    if contract is not None:
-        return contract['_id']
-    return db.nft_contracts.insert_one({
-        "address": address,
-        "abi": abi,
-        "events": [],
-    }).inserted_id
+    if contract is None:
+        db.nft_contracts.insert_one({
+            "address": address,
+            "abi": abi,
+        })
 
 
 def insert_event(db, address, event):
@@ -20,9 +18,13 @@ def insert_event(db, address, event):
     Insert an event into mongodb
     """
     contract_id = db.nft_contracts.find_one({'address': address})['_id']
-    db.nft_contracts.update_one(
-        {'_id': contract_id}, {"$push": {"events": event}}
-    )
+    db.nft_events.insert_one({
+        'address': address,
+        "name": event['event'],
+        "event": event,
+        "blockNumber":
+        event['blockNumber'],
+    })
 
 
 def get_last_scanned_event(db, address, default=9000000):
@@ -30,13 +32,14 @@ def get_last_scanned_event(db, address, default=9000000):
     Return the block number the last event
     was added at.
     """
-    event = db.nft_contracts.find_one({
-        'address': address,
-    })
+    event = db.nft_events.find_one({
+        "$query": {'address': address},
+        "$orderby": {"blockNumber": -1}
+    }, )
     # Inneficient XXX
-    if not event['events']:
+    if event is None:
         return default
-    return event['events'][-1]['blockNumber']
+    return event['blockNumber']
 
 
 def get_all_contracts(db):
