a627b94bdac9cca6e7e99598875645115a70dc56
0xnivek <0xnivek@gmail.com>
2022-04-07 19:14:19 -0400

    Run linter and add upsert_transfer

--- web3indexer/crud.py
+++ web3indexer/crud.py
@@ -5,26 +5,30 @@ log = structlog.get_logger()
 
 
 def insert_if_not_exists(db, address, abi):
-    contract = db.nft_contracts.find_one({'address': address})
+    contract = db.nft_contracts.find_one({"address": address})
     if contract is None:
-        db.nft_contracts.insert_one({
-            "address": address,
-            "abi": abi,
-        })
+        db.nft_contracts.insert_one(
+            {
+                "address": address,
+                "abi": abi,
+            }
+        )
 
 
 def insert_event(db, address, event):
     """
     Insert an event into mongodb
     """
-    contract_id = db.nft_contracts.find_one({'address': address})['_id']
-    db.nft_events.insert_one({
-        'address': address,
-        "name": event['event'],
-        "event": event,
-        "blockNumber": event['blockNumber'],
-        "tokenId": event['args'].get('tokenId'),
-    })
+    contract_id = db.nft_contracts.find_one({"address": address})["_id"]
+    db.nft_events.insert_one(
+        {
+            "address": address,
+            "name": event["event"],
+            "event": event,
+            "blockNumber": event["blockNumber"],
+            "tokenId": event["args"].get("tokenId"),
+        }
+    )
 
 
 def get_last_scanned_event(db, address, default=9000000):
@@ -32,28 +36,33 @@ def get_last_scanned_event(db, address, default=9000000):
     Return the block number the last event
     was added at.
     """
-    event = db.nft_events.find_one({
-        "$query": {'address': address},
-        "$orderby": {"blockNumber": -1}
-    }, )
+    event = db.nft_events.find_one(
+        {"$query": {"address": address}, "$orderby": {"blockNumber": -1}},
+    )
     # Inneficient XXX
     if event is None:
         return default
-    return event['blockNumber']
+    return event["blockNumber"]
 
 
-def insert_transfer(db, transfer):
+def upsert_transfer(db, transfer):
     """
     Insert a transfer event into mongodb
     """
-    db.transfers.insert_one({
-        "from": transfer["from"],
-        "nft_contract": transfer["nft_contract"],
-        "to": transfer["to"],
-        "tokenId": transfer["tokenId"],
-        "transactionHash": transfer["transactionHash"],
-    })
+    db.transfers.find_one_and_update(
+        {"transaction_hash": transfer["transactionHash"]},
+        {
+            "$set": {
+                "from": transfer["from"],
+                "nft_contract": transfer["nft_contract"],
+                "to": transfer["to"],
+                "tokenId": transfer["tokenId"],
+                "transaction_hash": transfer["transactionHash"],
+            }
+        },
+        upsert=True,
+    )
 
 
 def get_all_contracts(db):
-    return db.nft_contracts.find({}, {'address': 1, 'abi': 1, '_id': 0})
+    return db.nft_contracts.find({}, {"address": 1, "abi": 1, "_id": 0})
--- web3indexer/processor.py
+++ web3indexer/processor.py
@@ -3,7 +3,7 @@ import json
 import structlog
 from web3 import Web3
 
-from .crud import insert_transfer
+from .crud import upsert_transfer
 from .task import Task, ScrapeTask
 from .utils import read_file
 
@@ -14,8 +14,9 @@ log = structlog.get_logger()
 ERC165_ABI = read_file("abi/ERC165.json")
 ERC721_ABI = read_file("abi/ERC721.json")
 ERC721_TRANSFER_TOPIC = "0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef"
-ERC_165_IDENTIFIER = "01ffc9a7";
-ERC_721_IDENTIFIER = "80ac58cd";
+ERC_165_IDENTIFIER = "01ffc9a7"
+ERC_721_IDENTIFIER = "80ac58cd"
+
 
 class BlockProcessor:
     """
@@ -37,20 +38,25 @@ class BlockProcessor:
                 if len(topics) == 4 and topics[0].hex() == ERC721_TRANSFER_TOPIC:
                     if self._supports_erc721(w3, log.address):
                         print("LOG:", log)
-                        _from = "0x{}".format(topics[1].hex()[26:])
-                        to = "0x{}".format(topics[2].hex()[26:])
-                        tokenId = int(topics[3].hex(), 16)
-                        transactionHash = log.transactionHash.hex()
-                        insert_transfer(self.db, {
-                            'from': _from,
-                            'nft_contract': log.address,
-                            'to': to,
-                            'tokenId': tokenId,
-                            'transactionHash': transactionHash
-                        })
-                        print("TRANSFER:", _from, to, tokenId)
+                        (_from, to, tokenId, transactionHash) = self._parse_erc721_transfer_log(log)
+                        upsert_transfer(
+                            self.db,
+                            {
+                                "from": _from,
+                                "nft_contract": log.address,
+                                "to": to,
+                                "tokenId": tokenId,
+                                "transactionHash": transactionHash,
+                            },
+                        )
 
-    
+    def _parse_erc721_transfer_log(self, log):
+        topics = log.topics
+        _from = "0x{}".format(topics[1].hex()[26:])
+        to = "0x{}".format(topics[2].hex()[26:])
+        tokenId = int(topics[3].hex(), 16)
+        transactionHash = log.transactionHash.hex()
+        return (_from, to, tokenId, transactionHash)
 
     def _supports_erc721(self, w3, contract_address):
         try:
